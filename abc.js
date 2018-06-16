91b4bf2a25871d63b22692a4bf63ad63f99a9ad0
sivadwivedula <shivadwivedula3@gmail.com>
2018-06-17 00:02:35 +0530

    Create abc.js

---
+++ abc.js
@@ -0,0 +1,25 @@
+var abcd=prompt();
+	function call(){
+	var y=document.getElementsByClassName("_3AwwN")[0];
+	var x=y.children[1].childElementCount;
+	var s=y.children[1].children[1].children[0].innerText
+	 if(x ==2&&s=="online"){
+	if(count==0){
+	var date = new Date();
+	var abc=date.getHours() + ":" +date.getMinutes();
+	console.log(abc);
+	
+	count=1;
+	var inner=document.getElementsByClassName("_2S1VP copyable-text selectable-text")[0];
+	inner.innerHTML=abcd;
+	inner.dispatchEvent(new InputEvent('input',{bubbles:true}))
+	var hello=document.getElementsByClassName("_3oju3")[0];
+	hello.lastChild.click();
+	}
+	}
+	else{
+	count=0;
+	}
+	}var count=0;
+	setInterval(call,2000);
+
