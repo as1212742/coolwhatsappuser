457eef8e653a008c0a5d136f444b221726040dbd
sivadwivedula <shivadwivedula3@gmail.com>
2018-06-17 00:19:20 +0530

    working

--- abc.js
+++ abc.js
@@ -1,13 +1,16 @@
-var abcd=prompt("message  to send  everytime they are online [leave blank if u dont want]");
+var abcd=prompt("type message if u want to send message everytime");
 	function call(){
 	var y=document.getElementsByClassName("_3AwwN")[0];
 	var x=y.children[1].childElementCount;
-	var s=y.children[1].children[1].children[0].innerText
+	var s="";
+if(x==2){
+	s=y.children[1].children[1].children[0].innerText
+}
 	 if(x ==2&&s=="online"){
 	if(count==0){
 	var date = new Date();
 	var abc=date.getHours() + ":" +date.getMinutes();
-	console.log("online: "+abc);
+	console.log(abc);
 	
 	count=1;
 	var inner=document.getElementsByClassName("_2S1VP copyable-text selectable-text")[0];
@@ -22,4 +25,3 @@ var abcd=prompt("message  to send  everytime they are online [leave blank if u d
 	}
 	}var count=0;
 	setInterval(call,1000);
-
